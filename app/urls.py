7140a9a22e95215df41a856939f565d2b3906380
Brock Major <brockmajor@gmail.com>
2020-06-16 21:41:43 -0400

    base html file with bootstrap css import

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
+app_name = 'app'
 urlpatterns = [
     path('', views.home, name='home'),
     path('exercises/', views.exercises, name='exercises'),

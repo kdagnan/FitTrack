479dee4e315cc40605be34022250e5e91f05e6ea
Jesus Gonzalez <gonzalez03t@gmail.com>
2020-06-22 16:52:10 -0400

    commit

--- app/models.py
+++ app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+#from django.contrib.auth.models import User
 
-# Create your models here.
+class Exercise(models.Model):
+    name = models.CharField(max_length=100)
+    code = models.IntegerField()
+    group = models.CharField(max_length=100)
+    group_code = models.IntegerField()
+    description = models.TextField()
+    image_link = models.TextField()
+
+
+
+

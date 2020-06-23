c3a94816250d2e2ee073c41c75d016313c5005df
briankoehler <briankoehler@ufl.edu>
2020-06-22 23:31:36 -0400

    Initial brian commit

--- app/models.py
+++ app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+class Food_Entry(models.Model):
+    time = models.DateTimeField(default=timezone.now)
+    description = models.CharField(max_length=300)
+    calories = models.IntegerField()
+
+    def __str__(self):
+        return self.description

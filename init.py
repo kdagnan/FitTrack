2683b980dac347916cc8c713bd4564464fe4b6cb
Brock Major <brockmajor@gmail.com>
2020-06-30 23:09:37 -0400

    added init / run python scripts

---
+++ init.py
@@ -0,0 +1,17 @@
+import os
+import sys
+
+
+def main():
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'FitTrack.settings')
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError as exc:
+        raise ImportError("Couldn't import Django.") from exc
+
+    execute_from_command_line(["manage.py", "makemigrations"])
+    execute_from_command_line(["manage.py", "migrate"])
+
+
+if __name__ == '__main__':
+    main()

803621ec1a459f73fe818cfcd2064bf198292b9c
David Wang <david220wang385@gmail.com>
2020-07-28 00:11:28 -0400

    Linked Exercise Recommender tool to Exercise Log, after clicking the Add To Exercise Log, it redirects to today's workout with the Exercise already added; Cleaned up template files for ExLog app; Removed exerciselog.html in main app directory (Exlog is a separate app that handles its own URL routing); Cleaned up script that imports exercises from json file to database and included instruction on how to run the file

--- jsonToDatabase.py
+++ jsonToDatabase.py
@@ -1,18 +1,12 @@
-#create new super user
-#python3 manage.py createsuperuser
-#enter username
-#enter Email
-#enter Password
+# Run this file with:
+# python manage.py shell < jsonToDatabase.py
 
-#add json file to database
-python3 manage.py shell 
->>> import json
->>> from app.models import Exercise
->>> with open('Exercises.json') as f:
-...     exer_json = json.load(f)
-...
->>> for exe in exer_json:
-...     exercise = Exercise(name=exe['name'], group=exe['group'], group_code=exe['group_code'], description=exe['description'], image_link=exe['image_link'], ex_id=exe['ex_id'])
-...     exercise.save()
-...
->>> exit()
+# Add Exercises from json file to database
+import json
+from app.models import Exercise
+with open('Exercises.json') as f:
+    exer_json = json.load(f)
+
+for exe in exer_json:
+    exercise = Exercise(name=exe['name'], group=exe['group'], group_code=exe['group_code'], description=exe['description'], image_link=exe['image_link'], ex_id=exe['ex_id'])
+    exercise.save()
